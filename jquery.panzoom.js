1e498851dd6a2307ae15a59c0d10d3ef30b11828
Timmy Willison <timmywillisn@gmail.com>
2013-03-14 16:25:21 -0400

    Add license

--- jquery.panzoom.js
+++ jquery.panzoom.js
@@ -7,7 +7,7 @@
  * Pinch gestures for zooming is supported where touch is supported
  * Copyright (c) 2012 timmy willison
  * Released under the MIT license
- * https://github.com/jquery/jquery/blob/master/MIT-LICENSE.txt
+ * https://github.com/timmywil/jquery.panzoom/blob/master/MIT-LICENSE.txt
  */
 
 (function( global, factory ) {
