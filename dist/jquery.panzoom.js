021febd7753603564bafccfc5cef1a8740cb5b07
Timmy Willison <timmywillisn@gmail.com>
2013-06-20 11:46:31 -0400

    Update dependencies in jQuery plugin manifest

--- dist/jquery.panzoom.js
+++ dist/jquery.panzoom.js
@@ -1,5 +1,5 @@
 /**
- * @license jquery.panzoom.js v1.1.0
+ * @license jquery.panzoom.js v1.1.1
  * Updated: Thu Jun 20 2013
  * Add pan and zoom functionality to any element
  * Copyright (c) 2013 timmy willison
