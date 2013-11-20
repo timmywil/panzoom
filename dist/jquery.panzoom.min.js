9fae89dc56dc28aed6898a760d69e7d3f3bb0591
Timmy Willison <timmywillisn@gmail.com>
2013-11-20 11:35:24 -0500

    Release 1.8.2

--- dist/jquery.panzoom.min.js
+++ dist/jquery.panzoom.min.js
@@ -1,5 +1,5 @@
 /**
- * @license jquery.panzoom.js v1.8.1
+ * @license jquery.panzoom.js v1.8.2
  * Updated: Wed Nov 20 2013
  * Add pan and zoom functionality to any element
  * Copyright (c) 2013 timmy willison
