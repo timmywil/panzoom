2b695796f1baf56c015c6cd233f40a23ecd27894
Timmy Willison <4timmywil@gmail.com>
2019-11-18 15:38:10 -0500

    build(demo): add log to clarify demo/panzoom.js purpose

--- demo/global-panzoom.ts
+++ demo/global-panzoom.ts
@@ -1,5 +1,8 @@
 import Panzoom from '../src/panzoom'
 
+console.log('This is a demo version of Panzoom for testing.')
+console.log('It exposes a global (window.Panzoom) and should not be used in production.')
+
 declare global {
   interface Window {
     Panzoom: typeof Panzoom
