418d10f1a80ec639fac54bd27e76b29882014e03
Daniel Blok <virtualpilot88@gmail.com>
2020-04-11 18:14:01 +0200

    feat(panzoom): add pan option - roundToPixels

--- src/types.ts
+++ src/types.ts
@@ -158,6 +158,8 @@ interface PanSpecificOptions {
   relative?: boolean
   /** Disable panning while the scale is equal to the starting value */
   panOnlyWhenZoomed?: boolean
+  /** Round transform-translate x and values to whole pixels. (Prevents images getting blurry)  */
+  roundToPixels?: boolean
 }
 
 interface ZoomSpecificOptions {
