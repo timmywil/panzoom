2ab54827d5ab75f828101eb05b262638fc840438
Timmy Willison <4timmywil@gmail.com>
2020-07-07 11:12:36 -0400

    docs(demo): remove unnecessary window declaration

--- demo/global-panzoom.ts
+++ demo/global-panzoom.ts
@@ -3,9 +3,4 @@ import Panzoom from '../src/panzoom'
 console.log('This is a demo version of Panzoom for testing.')
 console.log('It exposes a global (window.Panzoom) and should not be used in production.')
 
-declare global {
-  interface Window {
-    Panzoom: typeof Panzoom
-  }
-}
 window.Panzoom = Panzoom
