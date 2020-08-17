e6633b2bdfa84427448e7e351860d141ecf48b1d
Timmy Willison <4timmywil@gmail.com>
2020-08-17 13:37:23 -0400

    build: export an es module; add source and module fields to package
    Close gh-502

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,51 @@
 import typescript from 'rollup-plugin-typescript2'
-export default {
-  input: './src/panzoom.ts',
-  plugins: [
-    typescript({
-      tsconfigOverride: {
-        exclude: ['node_modules', 'test'],
-        compilerOptions: {
-          declaration: true
+
+const banner = `/**
+* Panzoom for panning and zooming elements using CSS transforms
+* Copyright Timmy Willison and other contributors
+* https://github.com/timmywil/panzoom/blob/master/MIT-License.txt
+*/`
+
+export default [
+  {
+    input: './src/panzoom.ts',
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          exclude: ['node_modules', 'test'],
+          compilerOptions: {
+            declaration: true
+          }
         }
-      }
-    })
-  ],
-  output: {
-    banner: `/**
- * Panzoom for panning and zooming elements using CSS transforms
- * Copyright Timmy Willison and other contributors
- * https://github.com/timmywil/panzoom/blob/master/MIT-License.txt
- */`,
-    format: 'umd',
-    name: 'Panzoom',
-    file: 'dist/panzoom.js'
+      })
+    ],
+    output: {
+      banner,
+      file: 'dist/panzoom.js',
+      format: 'umd',
+      name: 'Panzoom'
+    },
+    watch: {
+      include: './src'
+    }
   },
-  watch: {
-    include: './src'
+  {
+    input: './src/panzoom.ts',
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          exclude: ['node_modules', 'test']
+        }
+      })
+    ],
+    output: {
+      banner,
+      file: 'dist/panzoom.es.js',
+      format: 'es',
+      name: 'Panzoom'
+    },
+    watch: {
+      include: './src'
+    }
   }
-}
+]
