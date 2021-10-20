1f698095df71bac22dd7fe9e34a59978cb83e41f
Timmy Willison <4timmywil@gmail.com>
2021-10-20 15:27:09 -0400

    chore: release 4.4.2

---
+++ dist/src/panzoom.d.ts
@@ -0,0 +1,17 @@
+/**
+ * Panzoom for panning and zooming elements using CSS transforms
+ * https://github.com/timmywil/panzoom
+ *
+ * Copyright Timmy Willison and other contributors
+ * Released under the MIT license
+ * https://github.com/timmywil/panzoom/blob/main/MIT-License.txt
+ *
+ */
+import './polyfills';
+import { PanzoomObject, PanzoomOptions } from './types';
+declare function Panzoom(elem: HTMLElement | SVGElement, options?: Omit<PanzoomOptions, 'force'>): PanzoomObject;
+declare namespace Panzoom {
+    var defaultOptions: PanzoomOptions;
+}
+export { PanzoomObject, PanzoomOptions };
+export default Panzoom;
