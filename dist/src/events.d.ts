8a0fbef80c7515a22279520d15fc0f46ae259354
Timmy Willison <timmywil@users.noreply.github.com>
2025-12-02 17:42:20 -0500

    chore(release): remove dist after release

--- dist/src/events.d.ts
+++
@@ -1,8 +0,0 @@
-declare let events: {
-    down: string;
-    move: string;
-    up: string;
-};
-export { events as eventNames };
-export declare function onPointer(event: 'down' | 'move' | 'up', elem: HTMLElement | SVGElement | Document, handler: (event: PointerEvent) => void, eventOpts?: boolean | AddEventListenerOptions): void;
-export declare function destroyPointer(event: 'down' | 'move' | 'up', elem: HTMLElement | SVGElement | Document, handler: (event: PointerEvent) => void): void;
--- dist/src/isAttached.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Determine if an element is attached to the DOM
- * Panzoom requires this so events work properly
- */
-export default function isAttached(node: Node): boolean;
